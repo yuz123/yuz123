70319837a542552f2b47427f0aea37011b8c05ef
Yuriy Tkachenko <yuriy.tkachenko@musicqubed.com>
2013-01-08 17:03:37 +0200

    fixed

--- MajorDomo/src/com/yuz/webview/MainView.java
+++ MajorDomo/src/com/yuz/webview/MainView.java
@@ -12,7 +12,7 @@ import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
+
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
