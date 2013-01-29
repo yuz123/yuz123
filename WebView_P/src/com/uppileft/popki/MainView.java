d26c3b863602c1eea77246b4a1cb8e63c627b965
Yuriy Tkachenko <yuriy.tkachenko@musicqubed.com>
2013-01-29 15:34:24 +0200

    123

--- WebView_P/src/com/uppileft/popki/MainView.java
+++ WebView_P/src/com/uppileft/popki/MainView.java
@@ -156,7 +156,7 @@ public class MainView extends Activity {
 		editor.putInt("check_run_times", check_run_times);
 		editor.commit();
 		Intent browserIntent = new Intent(Intent.ACTION_VIEW,
-				Uri.parse("https://play.google.com/store/apps/details?id=com.uppileft.popki&feature=search_result#?t=W251bGwsMSwxLDEsImNvbS51cHBpbGVmdC50aXRzaG90Z2lybCJd"));
+				Uri.parse("https://play.google.com/store/apps/details?id=com.uppileft.popki&feature=more_from_developer#?t=W251bGwsMSwyLDEwMiwiY29tLnVwcGlsZWZ0LnBvcGtpIl0"));
 		startActivity(browserIntent);
 		finish();
 	}
