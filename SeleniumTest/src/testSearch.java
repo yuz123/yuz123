5371ec6646fc6b1142907c4b67794f6e383388aa
Yuriy_Tkachenko <Yuriy_Tkachenko@EPUAKYIW0672.kyiv.epam.com>
2013-03-21 10:50:47 +0200

    123

--- SeleniumTest/src/testSearch.java
+++ SeleniumTest/src/testSearch.java
@@ -1,16 +1,19 @@
-import java.util.concurrent.TimeUnit;
+import static org.junit.Assert.assertTrue;
 
-import junit.framework.Assert;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class testSearch {
 private FirefoxDriver driver;
+private static final Logger LOGGER = LoggerFactory.getLogger(testSearch.class);
 	/*open fb.com
 	enter login
 	enter pass
@@ -18,7 +21,7 @@ private FirefoxDriver driver;
 	@Before
 	public void setUp(){
 		driver = new FirefoxDriver();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 		driver.get("http://fb.com");
 	}
 	
@@ -30,8 +33,22 @@ private FirefoxDriver driver;
 	
 	@Test
 	public void test(){
-		HomePage home = new HomePage(driver);
-		MainPage main = home.login("","");
-		Assert.assertTrue(main.welcome("Добро пожаловать на Facebook");
+		//String pageSource = driver.getPageSource();
+	//	HomePage home = new HomePage(driver);
+	//	MainPage main = home.login("","");
+	//	assertEquals("Incorrect Email", 
+	//		     driver.findElement(By.xpath(".//*[@id='login_form']/div[1]/div[1]")).getText());
+		//driver.W(driver.findElementByXPath(".//*[@id='u_0_0']"));
+		WebElement checkboxFemale = driver.findElementByXPath(".//*[@id='u_0_0']");
+		WebElement checkboxMale = driver.findElementByXPath(".//*[@id='u_0_1']");
+		checkboxFemale.click();
+		assertTrue("checkboxFemale.isSelected", checkboxFemale.isSelected());
+		LOGGER.info("checkbox checked");
+		LOGGER.error("checkbox checked");
+		LOGGER.warn("checkbox checked");
+		assertTrue("checkboxMale.isSelected", !checkboxMale.isSelected());
+        /*
+         * System.out.println("checkboxMale.isSelected()"): " + checkboxMAle.isSe);
+         */
 	}
 }
