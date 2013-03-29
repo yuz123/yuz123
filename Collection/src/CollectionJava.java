69bef7b0abd11f5327c6d5ceb943442bbc0db7e2
Yuriy_Tkachenko <Yuriy_Tkachenko@EPUAKYIW0672.kyiv.epam.com>
2013-03-29 18:31:19 +0200

    Collections

--- Collection/src/CollectionJava.java
+++ Collection/src/CollectionJava.java
@@ -9,31 +9,68 @@ public class CollectionJava {
    
      */
     public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        
         int[] array = new int[5];
-        int i = 0;
-        for (i = 0; i <= 4; i++) {
-        System.out.println("Vvedite "+i+" element:");
-        String line = in.readLine();
-        array[i] = Integer.valueOf(line);
-        System.out.println(array[i]);
-        }
-        System.out.println("Vvedite 4islo dlya sravneniya");
-        String line = in.readLine();
-        int y = Integer.valueOf(line);
-        int times = 0;
-        for (i = 0; i <= 4; i++) {
-            System.out.println("Element #" + i + "= " + array[i]);
-            if (array[i] == y) {
-                System.out.println("Element #" + array[i] + " sovpadaet s vvedennim 4islom " + y);
-            times++;
-            }
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+       try{
+            System.out.println("Fill, show, include");
+            String line = in.readLine();
+            switch (line) {
+            case "f":
+                Stroka.fill(array);
+                break;
+            case "s":
+                Stroka.show(array);
+                break;
+            case "i":
+                Stroka.includeTimes(array);
+                break;
+            default:
+                System.out.println("Wrong! reenter");
+                break;
+            } 
+    }
+    catch(NumberFormatException ne){
+    System.out.println(ne.getMessage() + " is not a numeric value.");
+    System.exit(0);
+    }
             
-            }
-        System.out.println("Koli4estvo sovpadeniy = " + times);
+       // 
         System.out.println();
     }
-      
-
-
-}
+  
+    public static class Stroka {
+        
+        static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        
+        public static int[] fill(int[] array) throws IOException {
+            for (int i = 0; i <= 4; i++) {
+                System.out.printf("Vvedite %d element:", i);
+                String line = in.readLine();
+                array[i] = Integer.valueOf(line);
+                System.out.println(array[i]);
+                }
+            return array;
+        }
+        
+        public static void show(int[] array) {
+            for (int i = 0; i <= 4; i++) {
+                System.out.printf("Element #%d = %d%n", i, array[i]);
+            }
+        }
+        public static void includeTimes(int[] array) throws IOException {
+            int times = 0;
+            System.out.println("Vvedite 4islo dlya sravneniya");
+            String line = in.readLine();
+            int y = Integer.valueOf(line);
+            for (int i = 0; i <= 4; i++) {
+              System.out.printf("Element #%d = %d%n", i, array[i]);
+              if (array[i] == y) {
+                  System.out.printf("Element #%d sovpadaet s vvedennim 4islom %d", array[i], y);
+              times++;              
+              }  
+        }
+            System.out.println("Koli4estvo sovpadeniy = " + times);
+        }
+    }      
+  }
